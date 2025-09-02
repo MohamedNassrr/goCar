79537055634a60b5103d4b6b420102f33c56fc66
MohamedNasrr <mohamednasrrkh@gmail.com>
2025-09-02 16:54:55 +0300

    - add user current location to trip request screen

--- lib/client/features/home/presentation/views/widgets/select_car_bottom_sheet.dart
+++ lib/client/features/home/presentation/views/widgets/select_car_bottom_sheet.dart
@@ -1,57 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_car/client/core/utils/client_assets_data.dart';
 import 'package:go_car/client/core/widgets/custom_select_car_type.dart';
+import 'package:go_car/client/features/home/presentation/controller/geo_coding_cubit/geo_coding_cubit.dart';
+import 'package:go_car/client/features/home/presentation/controller/geo_coding_cubit/geo_coding_state.dart';
+import 'package:go_car/client/features/trip_request/presentation/views/trip_bottom_sheet.dart';
+import 'package:go_car/core/widgets/custom_circule_indicator.dart';
 
 class SelectCarBottomSheet extends StatelessWidget {
   const SelectCarBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      bottom: 0,
-      right: 0,
-      left: 0,
-      child: Container(
-        height: 220.h,
-        width: double.infinity,
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topRight: Radius.circular(30),
-            topLeft: Radius.circular(30),
-          ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            IconButton(
-              onPressed: () {},
-               icon: const Icon(Icons.close)
-               ),
-            Flexible(
-              child: CustomSelectCarType(
-                onPressed: () {},
-                carImage: ClientAssetsData.normalCar,
-                carTypeText: 'Saver',
-                destinationToPickUpTime: '2',
-                tripPrice: '180',
-                carTypeDescriptionText: 'Affordable, compact rides',
+    return BlocProvider(
+      create: (context) => GeoCodingCubit()..convertToString(),
+      child: BlocBuilder<GeoCodingCubit, GeoCodingState>(
+        builder: (context, state) {
+          String userLocation;
+          if (state is GeoCodingConvertedStates) {
+            userLocation = state.address;
+            return Positioned(
+              bottom: 0,
+              right: 0,
+              left: 0,
+              child: Container(
+                height: 220.h,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(30),
+                    topLeft: Radius.circular(30),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Flexible(
+                      child: CustomSelectCarType(
+                        onPressed: () {
+                          showModalBottomSheet(
+                            context: context,
+                            enableDrag: true,
+                            isDismissible: true,
+                            showDragHandle: true,
+                            builder:
+                                (context) =>
+                                    TripBottomSheet(userLocation: userLocation),
+                          );
+                        },
+                        carImage: ClientAssetsData.normalCar,
+                        carTypeText: 'Saver',
+                        destinationToPickUpTime: '2',
+                        tripPrice: '180',
+                        carTypeDescriptionText: 'Affordable, compact rides',
+                      ),
+                    ),
+                    Flexible(
+                      child: CustomSelectCarType(
+                        onPressed: () {},
+                        carImage: ClientAssetsData.comfortableCar,
+                        carTypeText: 'Comfortable',
+                        destinationToPickUpTime: '2',
+                        tripPrice: '200',
+                        carTypeDescriptionText:
+                            'Comfortable sedans, top - quality drivers',
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Flexible(
-              child: CustomSelectCarType(
-                onPressed: () {},
-                carImage: ClientAssetsData.comfortableCar,
-                carTypeText: 'Comfortable',
-                destinationToPickUpTime: '2',
-                tripPrice: '200',
-                carTypeDescriptionText:
-                    'Comfortable sedans, top - quality drivers',
-              ),
-            ),
-          ],
-        ),
+            );
+          } else {
+            return const CustomCircleIndicator();
+          }
+        },
       ),
     );
   }
